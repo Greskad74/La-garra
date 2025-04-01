b0d91e51e713cc20571b3e10b0374d857fad0bd6
Greskad74 <zayaman74@gmail.com>
2025-03-31 23:11:33 -0600

    correciones

--- src/librerias/gpio.c
+++ src/librerias/gpio.c
@@ -8,13 +8,13 @@ void init_gpio(void){
 
 	/***********pwm***************/
 	//PA0 mode 2MHz.
-	GPIOA->CRL |= (1 << 5);
-	GPIOA->CRL &= ~(1 << 4);
+	GPIOA->CRL |= (1 << 1);
+	GPIOA->CRL &= ~(1 << 0);
 	// PA6
 
 	//PA alternate function pushpull
-	GPIOA->CRL |= (1 << 7);
-	GPIOA->CRL &= ~(1 << 6);
+	GPIOA->CRL |= (1 << 3);
+	GPIOA->CRL &= ~(1 << 2);
 // Timer 3
 	GPIOA -> CRL |= (1 << 27);
 	GPIOA -> CRL &= ~(1 << 26);
